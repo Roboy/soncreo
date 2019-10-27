d2c0fb5ba621e889b1197ed8dce4a0b196d6c9e7
missxa <aliona.kharchenko@gmail.com>
2019-10-27 09:43:24 +0100

    baidu speech recognition & translation

---
+++ asr_json.py
@@ -0,0 +1,182 @@
+# coding=utf-8
+
+import sys
+import json
+import base64
+import time
+import os
+
+import httplib
+import md5
+import HTMLParser
+
+import re
+import random
+
+import pdb
+
+IS_PY3 = sys.version_info.major == 3
+
+if IS_PY3:
+    from urllib.request import urlopen
+    from urllib.request import Request
+    from urllib.error import URLError
+    from urllib.parse import urlencode
+    from urllib.parse import quote, quote_plus
+    timer = time.perf_counter
+else:
+    from urllib2 import urlopen
+    from urllib2 import Request
+    from urllib2 import URLError
+    from urllib import urlencode
+    from urllib import quote, quote_plus
+    if sys.platform == "win32":
+        timer = time.clock
+    else:
+        # On most other platforms the best timer is time.time()
+        timer = time.time
+
+API_KEY = os.environ.get('BAIDU_TTS_API_KEY')
+SECRET_KEY = os.environ.get('BAIDU_TTS_SECRET_KEY')
+
+BAIDU_TRANSLATE_ID = os.environ.get('BAIDU_TRANSLATE_ID')
+BAIDU_TRANSLATE_SECRET = os.environ.get('BAIDU_TRANSLATE_SECRET')
+
+
+
+# 需要识别的文件
+AUDIO_FILE = './audio/16k.pcm'  # 只支持 pcm/wav/amr 格式，极速版额外支持m4a 格式
+# 文件格式
+FORMAT = AUDIO_FILE[-3:]  # 文件后缀只支持 pcm/wav/amr 格式，极速版额外支持m4a 格式
+
+CUID = '123456PYTHON'
+# 采样率
+RATE = 16000  # 固定值
+
+# 普通版
+
+DEV_PID = 1536  # 1537 表示识别普通话，使用输入法模型。1536表示识别普通话，使用搜索模型。根据文档填写PID，选择语言及识别模型
+ASR_URL = 'http://vop.baidu.com/server_api'
+SCOPE = 'audio_voice_assistant_get'  # 有此scope表示有asr能力，没有请在网页里勾选，非常旧的应用可能没有
+
+# 极速版 打开注释的话请填写自己申请的appkey appSecret ，并在网页中开通极速版（开通后可能会收费）
+
+# DEV_PID = 80001
+# ASR_URL = 'http://vop.baidu.com/pro_api'
+# SCOPE = 'brain_enhanced_asr'  # 有此scope表示有极速版能力，没有请在网页里开通极速版
+
+# 忽略scope检查，非常旧的应用可能没有
+# SCOPE = False
+
+def TranslateByBaidu(text, appid, secretKey,fromLang = 'zh',toLang = 'en'):
+    httpClient = None
+    myurl = '/api/trans/vip/translate'
+    q = text
+    salt = random.randint(32768, 65536)
+
+    sign = appid+q+str(salt)+secretKey
+    m1 = md5.new()
+    m1.update(sign)
+    sign = m1.hexdigest()
+    myurl = myurl+'?appid='+appid+'&q='+quote(q)+'&from='+fromLang+'&to='+toLang+'&salt='+str(salt)+'&sign='+sign
+
+    try:
+        httpClient = httplib.HTTPConnection('api.fanyi.baidu.com')
+        httpClient.request('GET', myurl)
+
+        #response是HTTPResponse对象
+        response = httpClient.getresponse()
+        result = response.read()
+        data = json.loads(result)
+        return data["trans_result"][0]["dst"]
+    except Exception(e):
+        print(e)
+    finally:
+        if httpClient:
+            httpClient.close()
+
+class DemoError(Exception):
+    pass
+
+
+"""  TOKEN start """
+
+TOKEN_URL = 'http://openapi.baidu.com/oauth/2.0/token'
+
+
+def fetch_token():
+    params = {'grant_type': 'client_credentials',
+              'client_id': API_KEY,
+              'client_secret': SECRET_KEY}
+    post_data = urlencode(params)
+    if (IS_PY3):
+        post_data = post_data.encode( 'utf-8')
+    req = Request(TOKEN_URL, post_data)
+    try:
+        f = urlopen(req)
+        result_str = f.read()
+    except URLError as err:
+        print('token http response http code : ' + str(err.code))
+        result_str = err.read()
+    if (IS_PY3):
+        result_str =  result_str.decode()
+
+    print(result_str)
+    result = json.loads(result_str)
+    print(result)
+    if ('access_token' in result.keys() and 'scope' in result.keys()):
+        print(SCOPE)
+        if SCOPE and (not SCOPE in result['scope'].split(' ')):  # SCOPE = False 忽略检查
+            raise DemoError('scope is not correct')
+        print('SUCCESS WITH TOKEN: %s  EXPIRES IN SECONDS: %s' % (result['access_token'], result['expires_in']))
+        return result['access_token']
+    else:
+        raise DemoError('MAYBE API_KEY or SECRET_KEY not correct: access_token or scope not found in token response')
+
+"""  TOKEN end """
+
+if __name__ == '__main__':
+    token = fetch_token()
+
+    speech_data = []
+    with open(AUDIO_FILE, 'rb') as speech_file:
+        speech_data = speech_file.read()
+
+    length = len(speech_data)
+    if length == 0:
+        raise DemoError('file %s length read 0 bytes' % AUDIO_FILE)
+    speech = base64.b64encode(speech_data)
+    if (IS_PY3):
+        speech = str(speech, 'utf-8')
+    params = {'dev_pid': DEV_PID,
+              'format': FORMAT,
+              'rate': RATE,
+              'token': token,
+              'cuid': CUID,
+              'channel': 1,
+              'speech': speech,
+              'len': length
+              }
+    post_data = json.dumps(params, sort_keys=False)
+    # print post_data
+    req = Request(ASR_URL, post_data.encode('utf-8'))
+    req.add_header('Content-Type', 'application/json')
+    try:
+        begin = timer()
+        f = urlopen(req)
+        result_str = f.read()
+        print ("Request time cost %f" % (timer() - begin))
+    except URLError as err:
+        print('asr http response http code : ' + str(err.code))
+        result_str = err.read()
+
+    # if (IS_PY3):
+    result_str = str(result_str)
+    result = json.loads(result_str)['result'][0]
+    result = result.encode('utf-8')
+    # pdb.set_trace()
+    result_en = TranslateByBaidu(result, BAIDU_TRANSLATE_ID, BAIDU_TRANSLATE_SECRET)
+    print(result_en)
+    print(result_str)
+    with open("result.txt","w") as of:
+        of.write(result_str)
